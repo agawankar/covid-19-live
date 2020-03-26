24606ba68641530f2172d72fb1400c2ee688bc78
agawankar <agawankar1989@gmail.com>
2020-03-26 08:45:25 -0700

    Updates

---
+++ precache-manifest.b7ed4197cc89b0b3832fe55abf504ffa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cebb83c0cfbd3aa40c81973d6e0624a",
+    "url": "/covid-19-live/index.html"
+  },
+  {
+    "revision": "842ca6f61f32f3f78858",
+    "url": "/covid-19-live/static/css/main.faee3152.chunk.css"
+  },
+  {
+    "revision": "888c46170a688cbef201",
+    "url": "/covid-19-live/static/js/2.be25ba60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-live/static/js/2.be25ba60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842ca6f61f32f3f78858",
+    "url": "/covid-19-live/static/js/main.396fad79.chunk.js"
+  },
+  {
+    "revision": "b2da1b71fbebcc2e68ef",
+    "url": "/covid-19-live/static/js/runtime-main.92ab53d9.js"
+  },
+  {
+    "revision": "99ee8a3c02629a45df1c595c772ce318",
+    "url": "/covid-19-live/static/media/fifa-who-step-1.99ee8a3c.png"
+  },
+  {
+    "revision": "18b8322674b75b45496093804121c4ee",
+    "url": "/covid-19-live/static/media/fifa-who-step-2.18b83226.png"
+  },
+  {
+    "revision": "c78455a93aebc1d5356a7a33965f1b08",
+    "url": "/covid-19-live/static/media/fifa-who-step-3.c78455a9.png"
+  },
+  {
+    "revision": "dea998ddd13ec077e7c386494b6fab85",
+    "url": "/covid-19-live/static/media/fifa-who-step-4.dea998dd.png"
+  },
+  {
+    "revision": "8dd0155f46b366d2dba8af364ec77515",
+    "url": "/covid-19-live/static/media/fifa-who-step-5.8dd0155f.png"
+  }
+]);
